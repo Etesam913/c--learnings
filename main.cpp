3155207567c12fbc229d3678e9f48e362d7755c0
etesam <etesamansari13@gmail.com>
2023-12-16 20:49:13 -0500

    :sparkles: Added `add_word` func to `Trie` class

--- main.cpp
+++ main.cpp
@@ -12,16 +12,32 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <memory>
+#include <utility>
 
 struct TrieNode
 {
 	bool is_word{false};
-	std::unordered_map<char, std::vector<std::string>> children;
+	std::unordered_map<char, std::unique_ptr<TrieNode>> children;
+	char cur_char{};
+
+	TrieNode()
+	{
+	}
+
+	TrieNode(char letter, bool is_word_val = false)
+	{
+		cur_char = letter;
+		is_word = is_word_val;
+		std::cout << cur_char << '\n';
+	}
 };
 
 class Trie
 {
 private:
+	std::unique_ptr<TrieNode> root = std::make_unique<TrieNode>(TrieNode());
+
 public:
 	Trie(std::vector<std::string> words)
 	{
@@ -33,8 +49,20 @@ public:
 
 	void add_word(std::string word)
 	{
-
-		std::cout << "Added word: " << word << '\n';
+		auto curNode = root.get();
+		for (char ch : word)
+		{
+			if (curNode->children.count(ch))
+			{
+				curNode = curNode->children[ch].get();
+			}
+			else
+			{
+				curNode->children[ch] = std::make_unique<TrieNode>(ch);
+				curNode = curNode->children[ch].get();
+			}
+		}
+		curNode->is_word = true;
 	}
 };
 
